1874d8300095561c20780750de308b9d3434eaf2
bradd evans <braddevans123@gmail.com>
2022-06-19 04:30:48 +0100

    add a note in the [/_/] directory

--- routes/_/_index.js
+++ routes/_/_index.js
@@ -1,7 +1,10 @@
 const _indexController = {};
 
 _indexController.name = 'Frontend';
+
+// anything placed in the [/_/] directory will be placed at [/] on express
 _indexController.description = 'Endpoints For /*';
+
 _indexController.emoji = '💀';
 
 module.exports = _indexController;
